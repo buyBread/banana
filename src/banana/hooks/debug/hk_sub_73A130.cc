e700279c1971750cef184bbc6ba53dd2906933c1
buyBread <buymybreadalt@gmail.com>
2026-09-08 00:37:23 +0300

    banana: port back the shader fix for decals (ugly)

--- src/banana/hooks/debug/hk_sub_73A130.cc
+++ src/banana/hooks/debug/hk_sub_73A130.cc
@@ -1,3 +1,6 @@
+#include <array>
+#include <cstring>
+
 #include "banana/hooks/base.hh"
 #include "util/types.hh"
 
@@ -20,10 +23,74 @@ struct archive_directory_t {
 constexpr u32 game_apk_hash = 0x003A261A;
 constexpr u32 merged_apk_type = 25;
 
-class hk_sub_73A130 : public c_hook<hk_sub_73A130, void*(__fastcall*)(archive_directory_t* archive, void* edx, void* pack_slot)> {
+// scuffed port (bored)
+void patch_decal_shader(u8* shader) {
+    std::array<u8, 0x1E70> patched {};
+
+    std::memcpy(patched.data(),
+                shader,
+                0x1E70);
+
+    auto write_u32 = [&](size_t offset, u32 value) {
+        std::memcpy(patched.data() + offset,
+                    &value,
+                    sizeof(value));
+    };
+
+    write_u32(0x0004, 0x01F0FFFE);
+
+    std::memset(patched.data() + 0x07B7,
+                0,
+                0x07C8 - 0x07B7);
+
+    // steal bytes from compiler metadata for the two extra shader instructions
+    std::memcpy(patched.data() + 0x07B7,
+                "sekrit",
+                sizeof("sekrit") - 1);
+
+    std::memmove(patched.data() + 0x07C8,
+                 patched.data() + 0x07E8,
+                 0x1E70 - 0x07E8);
+
+    write_u32(0x1000, 0xA1000040);
+    write_u32(0x1010, 0xA1550040);
+
+    constexpr std::array<u32, 4>
+    add_r6 { 0x03000002,
+             0x80080006,
+             0x81FF0006,
+             0xA0AA0042, };
+
+    std::memmove(patched.data() + 0x19DC + sizeof(add_r6),
+                 patched.data() + 0x19DC,
+                 0x1E50 - 0x19DC);
+
+    std::memcpy(patched.data() + 0x19DC,
+                add_r6.data(),
+                sizeof(add_r6));
+
+
+    constexpr std::array<u32, 4>
+    add_r0 { 0x03000002,
+             0x80080000,
+             0x81FF0000,
+             0xA0AA0042, };
+
+    std::memmove(patched.data() + 0x1B68 + sizeof(add_r0),
+                 patched.data() + 0x1B68,
+                 0x1E60 - 0x1B68);
+
+    std::memcpy(patched.data() + 0x1B68,
+                add_r0.data(),
+                sizeof(add_r0));
+
+    std::memcpy(shader, patched.data(), patched.size());
+}
+
+DEFINE_HOOK(hk_sub_73A130, void*(__fastcall*)(archive_directory_t*, void*, void*)) {
 
 public:
-    hk_sub_73A130() : c_hook((void*)0x0073A130, "debug") {}
+    CONSTRUCT_HOOK(hk_sub_73A130, (void*)0x0073A130, "debug") {}
 
     static void* __fastcall detour(archive_directory_t* archive, void* edx, void* pack_slot) {
         void* result = m_original(archive, edx, pack_slot); // populate archive_asset_t.raw_data
@@ -40,8 +107,14 @@ public:
             if (asset->asset_hash != game_apk_hash || asset->asset_type != merged_apk_type)
                 continue;
 
-            // decal_basic.PCFX (0x2007A0) + desired byte (0x000C9C)
-            asset->raw_data[0x0020143C] = 0x4c;
+            auto* decal_basic = asset->raw_data + 0x002007A0;
+
+            decal_basic[0x0C9C] = 0x4C;
+
+            patch_decal_shader(decal_basic + 0x191C);
+            patch_decal_shader(decal_basic + 0x3F1C);
+            patch_decal_shader(decal_basic + 0x651C);
+            patch_decal_shader(decal_basic + 0x8B1C);
 
             break;
         }
@@ -50,4 +123,4 @@ public:
     }
 };
 
-hk_sub_73A130 hk {};
+CREATE_HOOK(hk_sub_73A130);
