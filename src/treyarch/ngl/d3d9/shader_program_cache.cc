a8ce7630919af4e5dce6092457fc08a6bddf5aaf
buyBread <buymybreadalt@gmail.com>
2026-09-13 15:04:17 +0300

    ngl: shader program cache uses dinkumware::hash_map

--- src/treyarch/ngl/d3d9/shader_program_cache.cc
+++ src/treyarch/ngl/d3d9/shader_program_cache.cc
@@ -3,7 +3,6 @@
 #include "treyarch/ngl/d3d9/device.hh"
 #include "treyarch/ngl/d3d9/shader_program_cache.hh"
 #include "treyarch/shared/hash/algo.hh"
-#include "treyarch/shared/memory/heap.hh"
 
 using namespace treyarch;
 using namespace treyarch::ngl;
@@ -28,7 +27,7 @@ u32 shader_bytecode_size(const DWORD* bytecode) {
     return (u32)(instruction - bytecode + 1) * sizeof(DWORD);
 }
 
-u32 mix_shader_crc(u32 crc) {
+u32 d3d9::shader_program_cache::shader_crc_traits::operator()(const u32 &crc) const noexcept {
     div_t division = std::div((i32)(crc ^ 0xDEADBEEFu), 127773);
 
     i32 mixed = 16807 * division.rem - 2836 * division.quot;
@@ -37,151 +36,6 @@ u32 mix_shader_crc(u32 crc) {
         mixed += I32_MAX;
 
     return (u32)mixed;
-}
-
-// the bucket count grows one at a time, so part of the table still uses the old mask
-template<typename T>
-u32 resolve_shader_bucket(const d3d9::shader_program_cache::program_cache<T> &cache,
-                                u32                                           crc) {
-
-    u32 bucket = mix_shader_crc(crc) & cache.mask;
-
-    if (cache.bucket_count <= bucket)
-        bucket -= (cache.mask >> 1) + 1;
-
-    return bucket;
-}
-
-template<typename T>
-d3d9::shader_program_cache::program_node<T>* find_shader_program(d3d9::shader_program_cache::program_cache<T> &cache,
-                                                                 u32                                           crc) {
-
-    u32 bucket = resolve_shader_bucket(cache, crc);
-    auto* node = cache.buckets.begin[bucket];
-    auto* end  = cache.buckets.begin[bucket + 1];
-
-    while (node != end && node->crc < crc)
-        node = node->next;
-
-    if (node != end && node->crc == crc)
-        return node;
-
-    return cache.programs.sentinel;
-}
-
-template<typename T>
-void refresh_shader_buckets(ngl::d3d9::shader_program_cache::program_cache<T> &cache) {
-    auto* sentinel = cache.programs.sentinel;
-
-    for (auto** bucket = cache.buckets.begin; bucket != cache.buckets.end; ++bucket)
-        *bucket = sentinel;
-
-    auto* node = sentinel->next;
-
-    for (u32 bucket = 0; bucket < cache.bucket_count; ++bucket) {
-        cache.buckets.begin[bucket] = node;
-
-        while (node != sentinel && resolve_shader_bucket(cache, node->crc) == bucket)
-            node = node->next;
-    }
-
-    cache.buckets.begin[cache.bucket_count] = sentinel;
-}
-
-template<typename T>
-void append_shader_program(d3d9::shader_program_cache::program_node<T>* sentinel,
-                           d3d9::shader_program_cache::program_node<T>* node) {
-
-    node->previous = sentinel->previous;
-    node->next     = sentinel;
-
-    sentinel->previous->next = node;
-    sentinel->previous       = node;
-}
-
-template<typename T>
-void split_shader_bucket(ngl::d3d9::shader_program_cache::program_cache<T> &cache) {
-    u32 split_bucket  = cache.bucket_count - (cache.mask >> 1) - 1;
-    auto* split_begin = cache.buckets.begin[split_bucket];
-    auto* split_end   = cache.buckets.begin[split_bucket + 1];
-
-    u32 vector_size = (u32)(cache.buckets.end - cache.buckets.begin);
-
-    if (vector_size - 1 <= cache.bucket_count) {
-        u32 new_mask        = vector_size * 2 - 3;
-        u32 new_vector_size = new_mask + 2;
-
-        auto** buckets = (ngl::d3d9::shader_program_cache::program_node<T>**)
-            memory::heap::allocate(new_vector_size * sizeof(ngl::d3d9::shader_program_cache::program_node<T>*));
-
-        memory::heap::free(cache.buckets.begin);
-
-        cache.buckets.begin    = buckets;
-        cache.buckets.end      = buckets + new_vector_size;
-        cache.buckets.capacity = buckets + new_vector_size;
-        cache.mask             = new_mask;
-    } else if (cache.mask < cache.bucket_count)
-        cache.mask = cache.mask * 2 + 1;
-
-    auto* sentinel = cache.programs.sentinel;
-    auto* node     = split_begin;
-    u32 split_size = 0;
-
-    for (auto* scan = split_begin; scan != split_end; scan = scan->next)
-        ++split_size;
-
-    for (u32 index = 0; index < split_size; ++index) {
-        auto* next = node->next;
-
-        if ((mix_shader_crc(node->crc) & cache.mask) != split_bucket) {
-            node->previous->next = node->next;
-            node->next->previous = node->previous;
-
-            append_shader_program(sentinel, node);
-        }
-
-        node = next;
-    }
-
-    ++cache.bucket_count;
-
-    refresh_shader_buckets(cache);
-}
-
-template<typename T>
-d3d9::shader_program_cache::program_node<T>* insert_shader_program(d3d9::shader_program_cache::program_cache<T> &cache,
-                                                                   u32                                           crc,
-                                                                   T*                                            program) {
-
-    if (cache.bucket_count <= cache.programs.size >> 2)
-        split_shader_bucket(cache);
-
-    u32 bucket = resolve_shader_bucket(cache, crc);
-    auto* node = cache.buckets.begin[bucket];
-    auto* end  = cache.buckets.begin[bucket + 1];
-
-    while (node != end && node->crc < crc)
-        node = node->next;
-
-    if (node != end && node->crc == crc)
-        return node;
-
-    auto* inserted = (ngl::d3d9::shader_program_cache::program_node<T>*)
-        memory::heap::allocate(sizeof(ngl::d3d9::shader_program_cache::program_node<T>));
-
-    inserted->next     = node;
-    inserted->previous = node->previous;
-    inserted->crc      = crc;
-    inserted->program  = program;
-
-    node->previous->next = inserted;
-    node->previous       = inserted;
-
-    ++cache.programs.size;
-
-    refresh_shader_buckets(cache);
-
-    return inserted;
 }
 
 template<typename T, typename create_program_t>
@@ -193,11 +47,11 @@ HRESULT create_shader_program(      d3d9::shader_program_cache::program_cache<T>
     u32 byte_count = shader_bytecode_size(bytecode);
     u32 crc        = hash::crc2(bytecode, byte_count, U32_MAX);
 
-    auto* node = find_shader_program(cache, crc);
+    auto* node = cache.find(crc);
 
-    if (node != cache.programs.sentinel) {
+    if (node != cache.end()) {
         // the cache owns this, don't AddRef it here
-        *output = node->program;
+        *output = node->value.value;
 
         return D3D_OK;
     }
@@ -205,7 +59,7 @@ HRESULT create_shader_program(      d3d9::shader_program_cache::program_cache<T>
     HRESULT result = create_program(bytecode, output);
 
     // yes, retail caches it even when creation fails
-    insert_shader_program(cache, crc, *output);
+    cache.insert(crc, *output);
 
     return result;
 }
--- src/treyarch/ngl/d3d9/shader_program_cache.hh
+++ src/treyarch/ngl/d3d9/shader_program_cache.hh
@@ -2,50 +2,51 @@
 
 #include <d3d9.h>
 
+#include "treyarch/shared/dinkumware/hash_map.hh"
 #include "util/macros/sanity_assert.hh"
 #include "util/memory_reference.hh"
 #include "util/types.hh"
 
 namespace treyarch { namespace ngl { namespace d3d9 { namespace shader_program_cache {
-    template<typename T>
-    struct program_node {
-        program_node* next;
-        program_node* previous;
-        u32           crc;
-        T*            program;
+    struct shader_crc_traits {
+        u32 operator()(const u32 &crc) const noexcept;
+
+        bool less(const u32 &left, const u32 &right) const noexcept {
+            return left < right;
+        }
     };
 
     template<typename T>
-    struct program_list {
-        u32              allocator_state;
-        program_node<T>* sentinel;
-        u32              size;
-    };
+    using program_pair = dinkumware::hash_pair<u32, T*>;
 
     template<typename T>
-    struct bucket_vector {
-        u32               allocator_state;
-        program_node<T>** begin;
-        program_node<T>** end;
-        program_node<T>** capacity;
-    };
+    using program_node = dinkumware::list_node<program_pair<T>>;
 
     template<typename T>
-    struct program_cache {
-        u8               traits;
-        u8               reserved_001[3];
-        program_list<T>  programs;
-        bucket_vector<T> buckets;
-        u32              mask;
-        u32              bucket_count;
+    using program_cache = dinkumware::hash_map<u32, T*, shader_crc_traits>;
+
+    template<typename T>
+    struct program_cache_layout {
+        u8                traits;
+        u8                reserved_001[3];
+        u32               list_allocator_state;
+        program_node<T>*  sentinel;
+        u32               size;
+        u32               vector_allocator_state;
+        program_node<T>** bucket_begin;
+        program_node<T>** bucket_end;
+        program_node<T>** bucket_capacity;
+        u32               mask;
+        u32               bucket_count;
     };
 
+    using vertex_program_pair  = program_pair<IDirect3DVertexShader9>;
     using vertex_program_node  = program_node<IDirect3DVertexShader9>;
-    using pixel_program_node   = program_node<IDirect3DPixelShader9>;
-    using vertex_program_list  = program_list<IDirect3DVertexShader9>;
-    using vertex_bucket_vector = bucket_vector<IDirect3DVertexShader9>;
     using vertex_program_cache = program_cache<IDirect3DVertexShader9>;
-    using pixel_program_cache  = program_cache<IDirect3DPixelShader9>;
+    using vertex_cache_layout  = program_cache_layout<IDirect3DVertexShader9>;
+
+    using pixel_program_node  = program_node<IDirect3DPixelShader9>;
+    using pixel_program_cache = program_cache<IDirect3DPixelShader9>;
 
     HRESULT create_vertex_program(const DWORD* bytecode, IDirect3DVertexShader9** output);
     HRESULT create_pixel_program(const DWORD* bytecode, IDirect3DPixelShader9** output);
@@ -55,30 +56,32 @@ namespace treyarch { namespace ngl { namespace d3d9 { namespace shader_program_c
         inline util::memory_reference<pixel_program_cache>  pixel_programs  { 0x01117128 };
     } // references
 
-    ASSERT_SIZEOF  (vertex_program_node,          0x10);
-    ASSERT_OFFSETOF(vertex_program_node, next,    0x00);
+    ASSERT_SIZEOF  (vertex_program_pair,        0x08);
+    ASSERT_OFFSETOF(vertex_program_pair, key,   0x00);
+    ASSERT_OFFSETOF(vertex_program_pair, value, 0x04);
+
+    ASSERT_SIZEOF  (vertex_program_node,           0x10);
+    ASSERT_OFFSETOF(vertex_program_node, next,     0x00);
     ASSERT_OFFSETOF(vertex_program_node, previous, 0x04);
-    ASSERT_OFFSETOF(vertex_program_node, crc,     0x08);
-    ASSERT_OFFSETOF(vertex_program_node, program, 0x0C);
+    ASSERT_OFFSETOF(vertex_program_node, value,    0x08);
 
-    ASSERT_SIZEOF  (pixel_program_node,          0x10);
-    ASSERT_OFFSETOF(pixel_program_node, program, 0x0C);
+    ASSERT_SIZEOF(vertex_program_cache, 0x28);
 
-    ASSERT_SIZEOF  (vertex_program_list,           0x0C);
-    ASSERT_OFFSETOF(vertex_program_list, sentinel, 0x04);
-    ASSERT_OFFSETOF(vertex_program_list, size,     0x08);
+    ASSERT_SIZEOF  (vertex_cache_layout,                         0x28);
+    ASSERT_OFFSETOF(vertex_cache_layout, traits,                 0x00);
+    ASSERT_OFFSETOF(vertex_cache_layout, list_allocator_state,   0x04);
+    ASSERT_OFFSETOF(vertex_cache_layout, sentinel,               0x08);
+    ASSERT_OFFSETOF(vertex_cache_layout, size,                   0x0C);
+    ASSERT_OFFSETOF(vertex_cache_layout, vector_allocator_state, 0x10);
+    ASSERT_OFFSETOF(vertex_cache_layout, bucket_begin,           0x14);
+    ASSERT_OFFSETOF(vertex_cache_layout, bucket_end,             0x18);
+    ASSERT_OFFSETOF(vertex_cache_layout, bucket_capacity,        0x1C);
+    ASSERT_OFFSETOF(vertex_cache_layout, mask,                   0x20);
+    ASSERT_OFFSETOF(vertex_cache_layout, bucket_count,           0x24);
 
-    ASSERT_SIZEOF  (vertex_bucket_vector,           0x10);
-    ASSERT_OFFSETOF(vertex_bucket_vector, begin,    0x04);
-    ASSERT_OFFSETOF(vertex_bucket_vector, end,      0x08);
-    ASSERT_OFFSETOF(vertex_bucket_vector, capacity, 0x0C);
-
-    ASSERT_SIZEOF  (vertex_program_cache,               0x28);
-    ASSERT_OFFSETOF(vertex_program_cache, traits,       0x00);
-    ASSERT_OFFSETOF(vertex_program_cache, programs,     0x04);
-    ASSERT_OFFSETOF(vertex_program_cache, buckets,      0x10);
-    ASSERT_OFFSETOF(vertex_program_cache, mask,         0x20);
-    ASSERT_OFFSETOF(vertex_program_cache, bucket_count, 0x24);
+    ASSERT_SIZEOF(pixel_program_node, 0x10);
 
     ASSERT_SIZEOF(pixel_program_cache, 0x28);
+
+    ASSERT_ALIGNOF(vertex_program_cache, vertex_cache_layout);
 }}}} // treyarch::ngl::d3d9::shader_program_cache
